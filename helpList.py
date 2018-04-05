cedd7c1ad56cfe35ae8825efc66cc3abffa13844
Belvedersky <qwink186@icloud.com>
2018-04-05 10:07:30 +0500

    1st commit

---
+++ helpList.py
@@ -0,0 +1,15 @@
+def helpList():
+    print(
+"""
+s - search torrent
+c - search by categories
+n - show new torrents
+h - help
+q - exit
+mac-torrent-download.net - w/o advertising
+This is simple python parser link      
+Optional you can use Add-ons to clean download:
+Chrome: https://chrome.google.com/webstore/detail/skip-shortest-ads/bhgkdnnlhmefhnkfilcaaibapeepkfok
+Firefox: https://addons.mozilla.org/en-US/firefox/addon/adskip-by-tote/?src=search
+Opera: use Chrome add-ons https://addons.opera.com/ru/extensions/details/install-chrome-extensions/  
+""")
