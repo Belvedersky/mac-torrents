e67e84e96ee87f955a3fb4013bd2e0b7ec2fd3c2
Eqip3u <eqip3u.dem@yandex.ru>
2018-04-11 02:22:19 +0500

    enter,refactoring,clearlistobject

--- Main.py
+++ Main.py
@@ -1,44 +1,47 @@
-from tkinter import *
-from search import *
+from tkinter import * 
+from search import * 
 
-class Parsing():
+class GUI_Function():
 
-    def button_parse(self):
-        userParse = text1.get("1.0", END)
-        self.torrentsList = Search(userParse[0:-1])
+    def torrentsParse(self, event):
+        userParse = textBoxParsing.get()
+        self.torrentsList = Search(userParse)
+        torrentsListBox.delete(0, END)
+        torrentPropertyListBox.delete(0,END)
         for torrent in self.torrentsList:
-            TorrentsListBox.insert(END, torrent.text)
+            torrentsListBox.insert(END, torrent.text)
         
-    def onselect(self, event):
+    def onselectTorrent(self, event):
         widget = event.widget
         index = int(widget.curselection()[0])
         linkTorrent = self.torrentsList[index].get("href")
         torrent, magnet = ParsingTorrent(linkTorrent)
-        TorrentBox.insert(END, torrent)
-        TorrentBox.insert(END, magnet)
+        self.printTorrentInListBox(torrent, magnet)
 
-parsingFunc = Parsing()
+    def printTorrentInListBox(self, torrent, magnet):
+        torrentPropertyListBox.delete(0,END)
+        torrentPropertyListBox.insert(END, torrent)
+        torrentPropertyListBox.insert(END, magnet)
+
+GUI_Function = GUI_Function()
 
 root = Tk()
 root.title("MacTorrentParser")
 root.geometry("1000x780+300+200")
 
-text1 = Text(root, height = 1, width = 13, font = "Arial 14", wrap = WORD )
-text1.pack()
+textBoxParsing = Entry(root)
+textBoxParsing.bind("<Return>", GUI_Function.torrentsParse)
+textBoxParsing.pack()
 
-button1 = Button(root, bg = "red", text ="Parse!", command = parsingFunc.button_parse)
-button1.pack()
+buttonParsing = Button(root, bg = "red", text ="Parse!", command = GUI_Function.torrentsParse).pack()
 
-TorrentsListBox = Listbox(root, height = 20, width = 100, selectmode = SINGLE)
-TorrentsListBox.bind('<<ListboxSelect>>', parsingFunc.onselect)
-TorrentsListBox.place(x = 20, y = 20)
+torrentsListBox = Listbox(root, height = 20, width = 100, selectmode = SINGLE)
+torrentsListBox.bind('<<ListboxSelect>>', GUI_Function.onselectTorrent)
+torrentsListBox.place(x = 20, y = 20)
+torrentsListBox.pack()
 
-TorrentBox = Listbox(root, height = 120, width = 50, selectmode = SINGLE)
-TorrentBox.place(x = 120, y = 120)
-
-TorrentsListBox.pack()
-TorrentBox.pack()
-
-torrentsList = ""
+torrentPropertyListBox = Listbox(root, height = 120, width = 50, selectmode = SINGLE)
+torrentPropertyListBox.place(x = 120, y = 120)
+torrentPropertyListBox.pack()
 
 root.mainloop()
--- search.py
+++ search.py
@@ -2,99 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 def Search(searchName):
-    mode = "search"
-    page = 1
-    maxPage = 1000
-    if (searchName == ""):
-        searchName = input(" Search: ")
+    currentPage = 1
     category = ""
     name = f"?s={searchName}"
-    torrentList = ParsingTorrents(name, page, maxPage, category, mode)
+    torrentList = ParsingTorrents(category, currentPage, name)
+
     return torrentList
 
-def ParsingTorrents(name, currentPage, maxPage, category, mode):
-    if int(currentPage) < 0 or int(currentPage) > int(maxPage):
-        print("Error number page go back...")
-        CommandUser("deadlock", name, 1, maxPage, category, mode)
+def ParsingTorrents(category, currentPage, name):
     link = f"https://mac-torrent-download.net/{category}page/{currentPage}/{name}"
     requestsall = requests.get(link)
     soup = BeautifulSoup(requestsall.content, "html.parser")
     torrents_search = soup.find_all("a",{"rel":"bookmark"})
 
     return torrents_search
-        
-def ParsingInformationForPagination(soup):
-    pages = soup.find("div",{"class":"pagination"}).next.get_text()
-    maxPage = int(soup.find("div",{"class":"pagination"}).next.get_text().split()[3])
-    return pages, maxPage
 
-def ShowCategoriesList():
-    link = "https://mac-torrent-download.net/"
-    print(" Wait...")
-    requestsall = requests.get(link)
-    soup = BeautifulSoup(requestsall.content, "html.parser")
-    categories = soup.find_all("li",{"class": "menu-item"}) 
-    item_torrent = 1 
 
-    for name in categories[:27]:
-        name_category = name.text 
-        print(f"{item_torrent}. {name_category}") 
-        item_torrent += 1 
-    
-    choise_torrent = int(input("\n Choice number category:")) 
-    link = categories[choise_torrent-1].next.get("href").split("/")
-    category = f"{link[3]}/{link[4]}/{link[5]}" 
-    return category
-
-def CommandUser(commandUser, name, currentPage, maxPage, category,mode): 
-    commandMenu = True
-    while commandMenu:
-        if commandUser == "n":
-            ParsingTorrents(name, currentPage + 1, maxPage, category, mode)
-        elif commandUser == "d":
-            ParsingTorrents(name, currentPage - 1, maxPage, category, mode)
-        elif commandUser[0] == "p":
-            pageChoiceUser = commandUser.split()
-            ParsingTorrents(name, int(pageChoiceUser[1]), maxPage, category, mode)
-        elif commandUser == "deadlock":
-            ParsingTorrents(name, currentPage, maxPage, category,mode)
-        elif commandUser == "x":
-            exit()
-        else:
-            break
-
-def PrintTorrentsList(torrentsList):
-    print(" Wait...")
-    item_torrent = 1
-    for linked in torrentsList:
-        print(f" {item_torrent}. {linked.text}")
-        item_torrent +=1
-
-def PrintHelpCommandInTorrentList():
-    #Справка по коммандам
-    print("""
- Enter number to choice download Torrent
- n - next page
- p [number page] - go to page number
- d - Previos page
-    """)
-        
 def ParsingTorrent(linkTorrent):
     #Парсинг выбранного торрента
     requestsUser = requests.get(linkTorrent)
     soup = BeautifulSoup(requestsUser.content, "html.parser")
-    #Получаем информацию о торренте
-    # linkInfo_th = soup.find_all('th', {"class": "cell"})
-    # linkInfo_td = soup.find_all('td', {"class": "cell"})
-
-    #list = 1
-    # print(" ----------------------")
-    # for info in linkInfo_th:
-    #     table_info = info.text
-    #     info_torrent = linkInfo_td[list].get_text()
-    #     print(f" {table_info}: {info_torrent}")
-    #     list += 1
-    # print(" ----------------------")
 
     #Получаем массив из двух объектов(в 1 будет Магнет ссылка, во втором ссылка на Торрент)
     arrayMagtenAndTorrent = soup.find_all('li', {"class": "btn-list"})
