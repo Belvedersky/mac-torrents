43cc6bbfd85f9682738ccefe82a58771f20647c5
Belvedersky <qwink186@icloud.com>
2018-04-16 10:00:47 +0500

    setup

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,request, redirect, url_for
+#from block import *
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
