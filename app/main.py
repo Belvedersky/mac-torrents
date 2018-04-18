ad945cd9d9fdc1d3088bc434452219b89ad73cd3
Eqip3u <eqip3u.dem@yandex.ru>
2018-04-18 15:23:29 +0500

    baseGenerations

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
 from flask import Flask,render_template,request, redirect, url_for
-from parser import *
+from parser import getSearch, torrentList
+
 app = Flask(__name__)
 
-
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    items = torrentList()  
+
+    if request.method == 'GET':   
+        return render_template('index.html', getMethod = 'GET')
+
     if request.method == 'POST':
         search = request.form['search']
-        getSearch(search=search)
-        return redirect(url_for('index'))         
-    return render_template('index.html', torrents = items )  
+        getSearch(search)
+        return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    torrentsList = torrentList()
+    return render_template('result.html', torrentsList = torrentsList)
+    
 
 if __name__ == '__main__':
-    app.run( port=80,debug=True)
+    app.run(port=5001)
--- app/parser.py
+++ app/parser.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+
 def getSearch(search):
     link = f"https://mac-torrent-download.net/?s={search}"
     return link
-
 
 def torrentList():
     link = getSearch(0)
@@ -13,16 +13,18 @@ def torrentList():
     torrents_search = soup.find_all("a",{"rel":"bookmark"})# search name and link
     torrents_img = soup.find_all("img",{"class":"attachment-thumb100 size-thumb100 wp-post-image"})# search image
     torrents_dat = soup.find_all("time",{"itemprop":"datePublished"})# search name and link
+
     torrents_name = [torName.text for torName in torrents_search] # name
     torrents_link = [torLink.get('href') for torLink in torrents_search] # link
     torrents_image = [torImg.get('srcset').rsplit('?')[0][:-4] for torImg in torrents_img] # image
-    torrents_data = [torDat.text for torDat in torrents_dat]
-    torrents = {'name': torrents_name, 'link': torrents_link, 'image' : torrents_image, 'date' : torrents_data}
-    #print(len(torrents_name),len(torrents_link),len(torrents_image[:-24]),len(torrents_data))
-    #print(len(torrents))
-    #return torrents_list
-    print('get!')
-    return torrents
+    torrents_date = [torDat.text for torDat in torrents_dat]
 
-# getSearch("adobe")
-# torrentList()
+    torrentList = list()
+    for i in range(0, 49):
+        torrentList.append(dict(name = torrents_name[i],
+                                link = torrents_link[i],
+                                image = torrents_image[i], 
+                                date = torrents_date[i]))
+
+    return torrentList
+
