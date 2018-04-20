e4b2a73c8875e2c6811fadc276518cddee209681
Eqip3u <eqip3u.dem@yandex.ru>
2018-04-20 11:48:37 +0500

    categories list add

--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
 from flask import Flask,render_template,request, redirect, url_for
 from parser import torrentList
-from categories import cat
+from categories import getCategories
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html', getMethod = 'GET',categories = cat())
+        return render_template('index.html', getMethod = 'GET', categories = getCategories())
 
     if request.method == 'POST':
         search = request.form['search']
         return redirect(url_for('result', search = search))
 
-@app.route('/categories/', methods=['GET', 'POST'])
-def categories():
-    categories = cat()
-    return render_template('categories.html', categories=categories)
+
+# @app.route('/categories/', methods=['GET', 'POST'])
+# def categories():
+#     return render_template('categories.html', categories=getCategories())
 
 
 @app.route('/search/', methods=['GET', 'POST'])
@@ -26,6 +27,6 @@ def result(page = 1):
     return render_template('result.html', torrentsList = torrentsList, searchResult = searchResult)
     
 if __name__ == '__main__':
-    app.run(port=5001, debug=True, use_reloader=True)
+    app.run(port=5001, use_reloader=True)
 
 
