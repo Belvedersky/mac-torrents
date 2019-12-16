2a860fec05ca8dc0d9bf4284d23168d6b28e30bd
Belvedersky <qwink186@icloud.com>
2019-12-16 22:52:47 +0300

    getTorrentList()

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@
  * mac-torrent-parser CLI app
  * Search,download and open .torrents app
  */
-
+/* eslint-disable require-jsdoc */
 /* eslint-disable no-trailing-spaces */
 // eslint-disable-next-line no-unused-vars
 
@@ -28,42 +28,50 @@ const args = arg({
   '--label': '--name',
 });
 
+function getTorrentList(data) {
+  const torrents = _.keyBy(data, 'title');
+  menu.selectTorrents(torrents).then((answers) => {
+    const name = answers.torrents; 
+    __.getTorrents(torrents[name].link)
+        .then((data) => {
+          menu.writeTorrentInfo(data);
+          return data;
+        })
+        .then((data) => {
+          menu.saveOrOpen().then((d) => {
+          // console.log(d, data, torrents);
+            if (d.action === 'Save and open on torrent app') {
+              __.saveAndOpenTorrent(data);
+            }
+            if (d.action === 'Only save torrent file') {
+              __.saveTorrents(data);
+            } else {
+              console.clear();
+              getTorrentList(torrents);
+            }
+          });
+        });
+  });
+}
+
+
 menu.main.then((answers) => {
   switch (answers.action) {
     case 'Get top-list torrents list':
       __.getLastTorrents().then((data) => {
-        const torrents = _.keyBy(data, 'title');
-        menu.selectTorrents(torrents).then((answers) => {
-          // console.log(torrents[answers.torrents]);
-          __.getTorrents(torrents[answers.torrents].link)
-              .then((data) => {
-                menu.writeTorrentInfo(data);
-                return data;
-              })
-              .then((data) => {
-                menu.saveOrOpen().then((d) => {
-                  // console.log(d, data, torrents);
-                  if (d.action === 'Save and open on torrent app') {
-                    __.saveAndOpenTorrent(data);
-                  }
-                  if (d.action === 'Only save torrent file') {
-                    __.saveTorrents(data);
-                  } else {
-                    __.getTorrents(torrents[answers.torrents].link);
-                  }
-                });
-              });
-        });
+        getTorrentList(data);
       });
       break;
 
     case 'Search torrent by name':
       // console.log('Search ->');
-      menu.findMenu().then((data)=>{
-        __.searchTorrents(data.search);
-        // console.log(data);
+      menu.findMenu().then((data) => {
+        __.searchTorrents(data.search).then((data) => {
+          getTorrentList(data);
+        });
       });
       break;
+			
     case 'Search torrent by categories':
       console.log('Select categories -> TODO');
       break;
