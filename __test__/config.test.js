b533ade849aefb261185fab7c941c843a6e9c046
Belvedersky <qwink186@icloud.com>
2020-04-29 16:36:37 +0300

    config test

--- __test__/config.test.js
+++ __test__/config.test.js
@@ -1,5 +1,39 @@
 /* eslint-disable no-undef */
-import config from '../dist/config';
+// import config from '../dist/config';
+const config = {
+  url: 'https://www.torrentmac.net',
+  divList: 'article.post',
+  post: [
+    {
+      title: 'h2 a',
+      link: 'h2 a@href',
+      image: 'img@src',
+      date: 'time',
+      category: 'span',
+      description: 'section.post_content',
+    },
+  ],
+  divTorrent: '#main',
+  torrent: {
+    title: '.appdesc',
+    time: 'time',
+    category: 'span',
+    size: 'table.torrentInfoTable',
+    file: 'a.download-btn@href',
+    description: 'section.post_content p',
+  },
+  category: {
+    Video: 'application/video',
+    'Graphics & Design': 'application/graphics-design',
+    Photography: 'application/photography',
+    Productivity: 'application/productivity',
+    Utilities: 'application/utilities',
+    Music: 'application/music',
+    Office: 'application/office',
+    Adobe: 'application/adobe',
+    Games: 'games',
+  },
+};
 
 const data = ['url', 'post', 'torrent', 'category', 'divTorrent', 'divList'];
 
